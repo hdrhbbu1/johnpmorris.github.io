de3cecadef68e1c35dbe41be9a306e22a8a0b027
John Morris <johnmorris1095@gmail.com>
2016-07-19 00:36:06 +1000

    hiding post info if there isnt any

--- wrappers/md.js
+++ wrappers/md.js
@@ -11,17 +11,22 @@ module.exports = React.createClass({
   render () {
     const post = this.props.route.page.data
     const dateReadible = moment(post.date, "YYYY-MM-DD").format("LL")
+    var postDetails;
+    if (post.date || post.category) {
+      postDetails = 
+        <p className="small medium">
+          {post.category}
+          {post.category && post.date ? ' - ' : '' }
+          {post.date ? dateReadible : ''}
+        </p>
+    }
     return (
       <div className="markdown">
           <div className="container">
             <DocumentTitle title={ `Johnm  - ${ post.title } - Designer, Thinker, Maker. ` || 'Johnm - Designer, Thinker, Maker.' }></DocumentTitle>
             <h1 className={`grey-95 ${ post.hideTitle ? 'hidden' : ''}`}>{post.title}</h1>
-            <p className="padding-b-5 small medium">
-              {post.category}
-              {post.category && post.date ? ' - ' : '' }
-              {post.date ? dateReadible : ''}
-            </p>
-            <div dangerouslySetInnerHTML={{ __html: post.body }} />
+            {postDetails}
+            <div className="padding-t-5" dangerouslySetInnerHTML={{ __html: post.body }} />
         </div>
       </div>
     )
