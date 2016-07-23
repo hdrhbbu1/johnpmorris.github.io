6c2c4f49f9eacfb73f290e8b39c9f04c9b357b5f
John Morris <johnmorris1095@gmail.com>
2016-07-23 16:04:42 +1000

    refactoring social & adding darkwater

---
+++ consts/social.js
@@ -0,0 +1,22 @@
+const social = [
+  { 
+    name: 'Twitter',
+    link: 'http://twitter.com/johnm__/'
+  },{
+    name: 'Dribbble',
+    link: 'http://dribbble.com/johnm__/'
+  },{
+    name: 'Github',
+    link: 'http://github.com/johnpmorris'
+  },{
+    name: 'Codepen',
+    link: 'http://codepen.com/johnm__/'
+  },{
+    name: 'Instagram',
+    link: 'http://instagram.com/johnm__/'
+  },{
+    name: 'Email Spam',
+    link: 'http://www.linkedin.com/in/johnphilipmorris'
+  }];
+
+export default social;
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,23 @@ import sortBy from 'lodash/sortBy'
 import access from 'safe-access'
 import moment from 'moment'
 import '../css/main.scss'
+import social from '../consts/social.js'
 
 export default class Sass extends React.Component {
   render () {
+    const socialLinks = []
+    const socialPrint =
+      social.forEach((social) => {
+        socialLinks.push(
+          <p key={social.link} className="margin-b-1 medium external-link">
+            <a target="_blank" href={social.link}>
+              {social.name}
+            </a>
+          </p>
+        )  
+      })
+
     const pageLinks = []
-    // Sort pages.
     const sortedPages = sortBy(this.props.route.pages, (page) =>
       access(page, 'data.date')
     ).reverse()
@@ -33,19 +45,26 @@ export default class Sass extends React.Component {
     return (
       <div>
         <DocumentTitle title={ 'Johnm - Designer, Thinker, Maker.' }></DocumentTitle>
+
         <div className="container grey-95 padding-b-5 sm-padding-b-20">
           <h1>I use colours, letters and pictures to help people understand things. </h1> 
           <p className="italic serif"> 
             Don't hesitate to 
-            <Link className="italic margin-l-1" to={prefixLink('contact/')}>Get in touch </Link>
+            <Link className="italic margin-l-1" to={prefixLink('contact/')}>
+              Get in touch 
+            </Link>
           </p>
         </div>
+
         <div className="row sm-row padding-t-2">
           <div className="column sm-column-40 sm-padding-r-6">
             <p className="medium grey-95"> Things I've Made </p>
 
             <a className="medium external-link" href="http://noat.me" target="_blank">Noat</a>
             <p className="small"> A barebones note taking / to-do list app.</p>
+            
+            <a className="medium external-link" href="https://www.npmjs.com/package/hyperterm-darkwater" target="_blank">Hyperterm-darkwater</a>
+            <p className="small">A dark, flat, pastel theme for hyperterm</p>
 
             <a className="medium" href={prefixLink('readdit')}>Readdit</a>
             <p className="small"> Playing with Reddit's json api.</p>
@@ -58,21 +77,18 @@ export default class Sass extends React.Component {
           </div>
 
           <div className="column sm-column-40 sm-padding-r-6 padding-t-5 sm-padding-t-0">
-            <p className="medium grey-95"> Things I've written </p>
+            <p className="medium grey-95"> 
+              Things I've written 
+            </p>
             {pageLinks}
           </div>
 
           <div className="column padding-t-6 padding-b-6 sm-padding-t-0 sm-right">
-            <p className="margin-b-1 medium external-link"><a target="_blank" href="http://dribbble.com/johnm__/">Dribbble</a></p>
-            <p className="margin-b-1 medium external-link"><a target="_blank" href="http://instagram.com/johnm__/">Instagram</a></p>
-            <p className="margin-b-1 medium external-link"><a target="_blank" href="http://twitter.com/johnm__/">Twitter</a></p>
-            <p className="margin-b-1 medium external-link"><a target="_blank" href="http://github.com/johnpmorris">Github</a></p>
-            <p className="margin-b-1 medium external-link"><a target="_blank" href="http://codepen.io/Johnm__/">Codepen</a></p>
-            <p className="margin-b-1 medium external-link"><a target="_blank" href="http://www.linkedin.com/in/johnphilipmorris">Email spam</a></p>
+            {socialLinks}
           </div>
+
         </div>
       </div>
     )
   }
 }
-
